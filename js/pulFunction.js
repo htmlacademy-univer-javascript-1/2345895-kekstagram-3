d64b80ac8837840e773c6e8e401209e5c0f37020
Dmitry Tuchkov <dimonussit5@gmail.com>
2023-03-18 11:19:14 +0300

    homework5

--- js/pulFunction.js
+++ js/pulFunction.js
@@ -1,6 +1,10 @@
-function haslegalString(string, length) {
+
+
+function maxLineString(string, length) {
   return string.length <= length;
 }
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+export { getRandomNumber };
+export { maxLineString };
