0a9318963b6851ecac80a65edac7f6755585d2ae
Dmitry Tuchkov <dimonussit5@gmail.com>
2023-03-25 11:41:23 +0300

    fix

--- js/hedFunction.js
+++ js/hedFunction.js
@@ -11,7 +11,7 @@ function generatePhotos() {
       likes: getRandomNumber(15, 200),
       comments: getRandomNumber(0, 200)
     };
-    photos [i-1] = photo;
+    photos [i - 1] = photo;
   }
   return photos;
 }
