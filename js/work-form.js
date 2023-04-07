83a789b0fcb1657d650f72657dac97e26edf5a30
Dmitry Tuchkov <dimonussit5@gmail.com>
2023-04-07 23:27:50 +0300

    d9

--- js/work-form.js
+++ js/work-form.js
@@ -2,7 +2,7 @@ const fileInput = document.querySelector('#upload-file');
 const photoEditor = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('#upload-cancel');
 const imageForm = document.querySelector('.img-upload__form');
-const editablePhoto = document.querySelector('#editable-photo');
+const editablePhoto = document.querySelector('#ed-fot');
 
 fileInput.addEventListener('change', function() {
   photoEditor.classList.remove('hidden');
@@ -13,6 +13,8 @@ function resetImageEditor() {
   photoEditor.classList.add('hidden');
   document.body.classList.remove('modal-open');
   imageForm.reset();
+  editablePhoto.removeAttribute('style');
+  editablePhoto.removeAttribute('class');
 }
 
 document.addEventListener('keyup', function(event) {
@@ -24,3 +26,44 @@ document.addEventListener('keyup', function(event) {
 closeButton.addEventListener('click', function() {
   resetImageEditor();
 });
+
+const zoomInButton = document.querySelector('.scale__control--bigger');
+const zoomOutButton = document.querySelector('.scale__control--smaller');
+const scaleValue = document.querySelector('.scale__control--value');
+
+function zoomInPhoto() {
+  const scale = Number(scaleValue.value.replace('%', ''));
+  if (scale !== 100) {
+    const newScale = scale + 25;
+    editablePhoto.style = `transform: scale(${newScale / 100})`;
+    scaleValue.value = `${newScale}%`;
+  }
+}
+
+function zoomOutPhoto() {
+  const scale = Number(scaleValue.value.replace('%', ''));
+  if (scale !== 25) {
+    const newScale = scale - 25;
+    editablePhoto.style = `transform: scale(${newScale / 100})`;
+    scaleValue.value = `${newScale}%`;
+  }
+}
+
+const effectButtons = imageForm.querySelectorAll('.effects__radio');
+let checkedButton = imageForm.querySelector('#effect-none');
+function effectChange(evt) {
+  const pressedButton = evt.target;
+  if (pressedButton !== checkedButton) {
+    checkedButton.checked = false;
+    pressedButton.checked = true;
+    editablePhoto.classList.add(`effects__preview--${pressedButton.value}`);
+    editablePhoto.classList.remove(`effects__preview--${checkedButton.value}`);
+    checkedButton = pressedButton;
+  }
+}
+effectButtons.forEach((button) => {
+  button.addEventListener('click', effectChange);
+});
+
+zoomInButton.addEventListener('click', zoomInPhoto);
+zoomOutButton.addEventListener('click', zoomOutPhoto);
