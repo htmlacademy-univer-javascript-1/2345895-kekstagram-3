2a33b9752aa01550591face916a412acd6793fe4
Dmitry Tuchkov <dimonussit5@gmail.com>
2023-04-07 22:10:26 +0300

    mod8

---
+++ js/work-form.js
@@ -0,0 +1,28 @@
+const fileInput = document.querySelector('#upload-file');
+const photoEditor = document.querySelector('.img-upload__overlay');
+const closeButton = document.querySelector('#upload-cancel');
+const imageForm = document.querySelector('.img-upload__form');
+const editablePhoto = document.querySelector('#editable-photo');
+
+fileInput.addEventListener('change', function() {
+photoEditor.classList.remove('hidden');
+document.body.classList.add('modal-open');
+});
+
+function resetImageEditor() {
+photoEditor.classList.add('hidden');
+document.body.classList.remove('modal-open');
+imageForm.reset();
+editablePhoto.removeAttribute('style');
+editablePhoto.removeAttribute('class');
+}
+
+document.addEventListener('keyup', function(event) {
+if (event.key === 'Escape') {
+resetImageEditor();
+}
+});
+
+closeButton.addEventListener('click', function() {
+resetImageEditor();
+});
